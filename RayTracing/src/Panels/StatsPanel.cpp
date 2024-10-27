90520070744b045607015279d1fd7356da89fd40
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-10-27 12:49:12 +0100

    Refactor code to be more readable

--- RayTracing/src/Panels/StatsPanel.cpp
+++ RayTracing/src/Panels/StatsPanel.cpp
@@ -9,11 +9,13 @@ StatsPanel::StatsPanel(Renderer& renderer, float& lastRenderTime, bool& showStat
 bool StatsPanel::OnUIRender() {
 	if (m_ShowStatsPanel) {
 		ImGui::Begin("Stats", &m_ShowStatsPanel);
+
 		ImGui::BeginChild("Stats", ImVec2(0, 0), true);
 		ImGui::Text("Last render: %.3fms", m_LastRenderTime);
 		ImGui::Text("FPS: %.0f", ImGui::GetIO().Framerate);
 		ImGui::Text("Accumulated frames: %i", m_Renderer.GetFrameIndex());
 		ImGui::EndChild();
+
 		ImGui::End();
 	}
 
