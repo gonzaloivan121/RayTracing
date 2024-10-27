9b3b0edad1755b8727a3224760ab8c59554f24ed
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-10-27 13:16:14 +0100

    Better UI for the Settings Panel

--- RayTracing/src/Panels/SettingsPanel.cpp
+++ RayTracing/src/Panels/SettingsPanel.cpp
@@ -18,24 +18,34 @@ bool SettingsPanel::OnUIRender() {
 
 		ImGui::Begin("Settings", &m_ShowSettingsPanel);
 
+		ImGui::Separator();
+		ImGui::AlignTextToFramePadding();
+		Walnut::UI::TextCentered("Renderer Settings");
+		ImGui::Separator();
+
 		ImGui::BeginChild("Boolean Settings", ImVec2(0, 128), true);
 		ImGui::Checkbox("Accumulate", &m_Renderer.GetSettings().Accumulate);
 		ImGui::Checkbox("Multithreading", &m_Renderer.GetSettings().Multithreading);
 		ImGui::Checkbox("Fast Random", &m_Renderer.GetSettings().FastRandom);
 		ImGui::EndChild();
 
+		ImGui::BeginChild("Slider Settings", ImVec2(0, 90), true);
+		ImGui::DragInt("Ray Bounces", &m_Renderer.GetSettings().RayBounces, 1, 2, std::numeric_limits<int>::max());
+		ImGui::SliderInt("Resolution Scale", &m_Renderer.GetSettings().ResolutionScale, 1, 100, "%d%%", ImGuiSliderFlags_AlwaysClamp);
+		ImGui::EndChild();
+
 		if (m_Renderer.GetSettings().FastRandom) {
+			ImGui::Separator();
+			ImGui::AlignTextToFramePadding();
+			Walnut::UI::TextCentered("Fast Random Settings");
+			ImGui::Separator();
+
 			ImGui::BeginChild("Fast Random Settings", ImVec2(0, 128), true);
 			ImGui::Checkbox("Use Clock Time", &m_Renderer.GetSettings().UseClockTime);
 			ImGui::Checkbox("Use Frame Index", &m_Renderer.GetSettings().UseFrameIndex);
 			ImGui::Checkbox("Use Ray Bounces", &m_Renderer.GetSettings().UseRayBounces);
 			ImGui::EndChild();
 		}
-
-		ImGui::BeginChild("Slider Settings", ImVec2(0, 90), true);
-		ImGui::DragInt("Ray Bounces", &m_Renderer.GetSettings().RayBounces, 1, 2, std::numeric_limits<int>::max());
-		ImGui::SliderInt("Resolution Scale", &m_Renderer.GetSettings().ResolutionScale, 1, 100, "%d%%", ImGuiSliderFlags_AlwaysClamp);
-		ImGui::EndChild();
 
 		if (ImGui::Button("Reset Accumulation", ImVec2(ImGui::GetContentRegionAvail().x, 0))) {
 			resetFrameIndex = true;
