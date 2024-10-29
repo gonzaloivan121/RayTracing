f7c08ea4d7adbae489a59a121175356971ba7f87
Gonzalo Iván Chaparro Barese <gonzaloivan121@gmail.com>
2024-10-29 03:23:17 +0100

    Added new TranslationSerializer and TranslationService

---
+++ RayTracing/src/Translation/TranslationService.cpp
@@ -0,0 +1,21 @@
+#include "TranslationService.h"
+
+#include "Serializer/TranslationSerializer.h"
+
+#include "Walnut/Core/Log.h"
+
+#include <filesystem>
+
+void TranslationService::Use(std::string language) {
+	spdlog::info("TranslationService - Loading " + language + " translations");
+	s_CurrentLanguage = language;
+
+	std::filesystem::path languagePath(s_LanguagesPath + s_CurrentLanguage + ".yaml");
+
+	TranslationSerializer serializer(s_Data);
+	if (serializer.Deserialize(languagePath)) {
+		spdlog::info("TranslationService - " + language + " translations loading complete");
+	} else {
+		spdlog::error("TranslationService - There's been an error loading " + language + " translations");
+	}
+}
